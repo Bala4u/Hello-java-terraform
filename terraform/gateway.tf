9c1c184b8b3baf3af87a267b51c61452b706c83a
Bala4u <59081014+Bala4u@users.noreply.github.com>
2020-02-11 12:30:53 +0530

    Update gateway.tf

--- terraform/gateway.tf
+++ terraform/gateway.tf
@@ -6,18 +6,18 @@ resource "aws_api_gateway_rest_api" "hello_world_api" {
 resource "aws_api_gateway_resource" "hello_world_api_gateway" {
   rest_api_id = aws_api_gateway_rest_api.hello_world_api.id
   parent_id   = "aws_api_gateway_rest_api.hello_world_api.root_resource_id"
-  path_part   = "var.api_path"
+  path_part   = var.api_path
 }
 
 resource "aws_api_gateway_method" "hello_world_method" {
-  rest_api_id   = "aws_api_gateway_rest_api.hello_world_api.id"
+  rest_api_id   = aws_api_gateway_rest_api.hello_world_api.id
   resource_id   = "aws_api_gateway_resource.hello_world_api_gateway.id"
   http_method   = var.hello_world_http_method
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "hello_world_integration" {
-  rest_api_id             = "aws_api_gateway_rest_api.hello_world_api.id"
+  rest_api_id             = aws_api_gateway_rest_api.hello_world_api.id
   resource_id             = "aws_api_gateway_resource.hello_world_api_gateway.id"
   http_method             = "aws_api_gateway_method.hello_world_method.http_method"
   integration_http_method = "aws_api_gateway_method.hello_world_method.http_method"
@@ -27,7 +27,7 @@ resource "aws_api_gateway_integration" "hello_world_integration" {
 }
 
 resource "aws_api_gateway_method_response" "200" {
-  rest_api_id = "aws_api_gateway_rest_api.hello_world_api.id"
+  rest_api_id = aws_api_gateway_rest_api.hello_world_api.id
   resource_id = "aws_api_gateway_resource.hello_world_api_gateway.id"
   http_method = "aws_api_gateway_method.hello_world_method.http_method"
 
